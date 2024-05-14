84966dcdd64455c74727f6014f064faee67f48f5
Samuel Udoh <saudexgpt@gmail.com>
2024-05-14 16:46:59 +0100

    update

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,7 +8,7 @@ use App\Models\User;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Carbon;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Arr;
 class UsersController extends Controller
 {
