689454bf9cc7d2ccf1211a4ee405d35cf7abf1f2
Samuel Udoh <saudexgpt@gmail.com>
2024-05-21 12:52:00 +0100

    update

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -80,9 +80,9 @@ class ReportsController extends Controller
     public function adminDataAnalysisDashbord(Request $request)
     {
         $user = $this->getUser();
-        // if($user->role != 'staff') {
-        //     return response()->json(['message' => 'You are not a super admin'], 403);
-        // }
+        if($user->role != 'staff') {
+            return response()->json(['message' => 'You are not a super admin'], 403);
+        }
         $today = date('Y-m-d', strtotime('now'));
         $total_clients = Client::count();
 
