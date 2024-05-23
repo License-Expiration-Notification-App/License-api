96ef9f9b06195dfdcd339f354ec2a8d4d63ab449
Samuel Udoh <saudexgpt@gmail.com>
2024-05-23 17:47:41 +0100

    Bug fixes

--- app/Models/LicenseActivity.php
+++ app/Models/LicenseActivity.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class LicenseActivity extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
     protected $fillable = [
+        'client_id',
         'license_id',
         'title',
         'description',
