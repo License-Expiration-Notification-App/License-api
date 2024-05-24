73c8092c4c3e0d0e30d2ee4f90a67ed19d57cb96
Samuel Udoh <saudexgpt@gmail.com>
2024-05-24 13:00:58 +0100

    update

--- app/Http/Controllers/LicensesController.php
+++ app/Http/Controllers/LicensesController.php
@@ -390,7 +390,7 @@ class LicensesController extends Controller
     public function licenseUpcomingActivities(Request $request, License $license)
     {
         $upcoming_activities = LicenseActivity::where('license_id', $license->id)
-        ->where('status', 'Pending')->select('id', 'uuid', 'title', 'due_date', 'status', 'created_at')->paginate(10);
+        ->where('status', 'Pending')->select('id', 'uuid', 'title', 'due_date', 'status', 'type', 'created_at')->paginate(10);
         return response()->json(compact('upcoming_activities'), 200);
     }
     /**
