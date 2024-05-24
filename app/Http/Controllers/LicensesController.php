52a21d2f618819e3461c2e8f002456da1a52939e
Samuel Udoh <saudexgpt@gmail.com>
2024-05-24 11:22:00 +0100

    Update license activity timeline

--- app/Http/Controllers/LicensesController.php
+++ app/Http/Controllers/LicensesController.php
@@ -371,7 +371,7 @@ class LicensesController extends Controller
         }
         
        $activity_timeline = $licenseActivityQuery->where('license_id', $license->id)
-       ->where('status', '!=', 'Pending')->get();
+       ->where('status', '!=', 'Pending')->select('license_id', 'title', 'description', 'updated_at as action_date', 'status', 'type', 'color_code', 'due_date', 'uuid')->get();
        foreach($activity_timeline as $time_line) {
         $type = $time_line->type;
         if ($type == 'License Renewal') {
